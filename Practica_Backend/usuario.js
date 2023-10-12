6e31be534c265bd573c05128c3643ddca314b3b6
AquinoTania <136201349+AquinoTania@users.noreply.github.com>
2023-10-12 10:22:14 -0300

    Add files via upload

---
+++ Practica_Backend/usuario.js
@@ -0,0 +1,119 @@
+require('rootpath')();
+const mysql = require('mysql');
+const configuracion = require("config.json");
+
+var connection = mysql.createConnection(configuracion.database);
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("base de datos conectada");
+    }
+});
+
+var usuario_db = {};
+
+
+
+
+// C = CREATE= app.post('/', createUser);
+usuario_db.create = function (usuario, funcallback) {
+    consulta = "INSERT INTO USUARIO (mail, nickname, clave, persona) VALUES (?,?,?,?);";
+    params = [usuario.mail, usuario.nickname, usuario.clave, usuario.persona];
+
+    connection.query(consulta, params, (err, detail_bd) => {
+        if (err) {
+
+            if (err.code == "ER_DUP_ENTRY") {
+                funcallback({
+                    mensajito: "el usuario ya fue registrado",
+                    detalle: err
+                });
+            } else {
+                funcallback({
+                    mensajito: "error diferente",
+                    detalle: err
+                });
+            }
+        } else {
+
+            funcallback(undefined, {
+                mensajito: "se creo el usaurio " + usuario.nickname,
+                detalle: detail_bd
+            });
+        }
+    });
+}
+
+//R = READ= app.get('/', getAll);
+usuario_db.getAll = function (funCallback) {
+    var consulta = 'SELECT * FROM USUARIO';
+    connection.query(consulta, function (err, rows) {
+        if (err) {
+            funCallback(err);
+            return;
+        } else {
+            funCallback(undefined, rows);
+        }
+    });
+}
+
+//U = UPDATE=app.put('/:id_usuario', updateUser);
+usuario_db.update = function (datos_usuario, id_usaurio, funcallback) {
+    params = [datos_usuario.mail, datos_usuario.nickname, datos_usuario.clave, id_usaurio]
+    consulta = "UPDATE USUARIO set mail = ?, nickname = ?, clave = ? WHERE id_usuario = ?;";
+
+    connection.query(consulta, params, (err, result) => {
+        if (err) {
+            if (err.code = "ER_TRUNCATED_WRONG_VALUE") {
+                funcallback({
+                    message: `el id de usuario es incorrecto, se espera un numero entero`,
+                    detail: err
+                });
+            } else {
+                funcallback({
+                    message: `error desconocido`,
+                    detail: err
+                });
+            }
+        } else {
+            if (result.affectedRows == 0) {
+                funcallback({
+                    message: "No existe un usuario que coincida con el criterio de busqueda",
+                    detail: result
+                });
+            } else {
+                funcallback(undefined, {
+                    message: `se actualizaron los datos del usuario ${id_usaurio}`,
+                    detail: result
+                });
+            }
+    
+        }
+    });
+
+
+
+}
+
+// D = DELETE= app.delete('/:id_usuario', deleteUser);
+usuario_db.borrar = function (id_usuario, retorno) {
+    consulta = "DELETE FROM USUARIO WHERE id_usuario = ?";
+    connection.query(consulta, id_usuario, (err, result) => {
+        if (err) {
+            retorno({ menssage: err.code, detail: err }, undefined);
+
+        } else {
+
+            if (result.affectedRows == 0) {
+                retorno(undefined, { message: "no se encontro el usaurio, ingrese otro id", detail: result });
+            } else {
+                retorno(undefined, { message: "usuario eliminado", detail: result });
+            }
+        }
+    });
+}
+
+
+
+module.exports = usuario_db;
---
+++ Practica_Backend/usuariocontroller.js
@@ -0,0 +1,69 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+var usuarioDb = require("model/usuario.js");
+
+// --------------------Rutas Usuario------------------------------------ 
+
+app.get('/', getAll);
+app.post('/', createUser);
+app.put('/:id_usuario', updateUser);
+app.delete('/:id_usuario', deleteUser);
+
+
+// -------------------------------------------------------- 
+
+
+function getAll(req, res) {
+    usuarioDb.getAll((err, resultado) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(resultado);
+        }
+    });
+}
+
+function createUser(req, res) {
+    let usuario = req.body;
+    usuarioDb.create(usuario, (err, resultado) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(resultado);
+        }
+    });
+}
+
+
+function updateUser(req, res) {
+    let datos_usuario = req.body; 
+    let id_usaurio = req.params.id_usuario 
+    usuarioDb.update(datos_usuario, id_usaurio, (err,resultado) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(resultado)
+        }
+    })
+}
+
+
+function deleteUser(req, res) {
+    usuarioDb.borrar(req.params.id_usuario, (err, result_model) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            if (result_model.detail.affectedRows == 0) {
+                res.status(404).send(result_model.message);
+            } else {
+                res.send(result_model.message);
+            }
+        }
+    });
+}
+
+module.exports = app;
